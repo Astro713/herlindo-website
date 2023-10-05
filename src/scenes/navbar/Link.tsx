d945e6f09fc9bbd92d2ab99c1517548ef2e08282
Alexis <astrohitters@yahoo.com>
2023-10-04 19:28:19 -0500

    menu toggle fix

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -2,28 +2,39 @@ import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
-    page: string;
-    selectedPage: SelectedPage;
-    setSelectedPage: (value: SelectedPage) => void;
+  page: string;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+  isMobileMenuOpen?: boolean; // Add a ? to make it an optional prop
+  closeMobileMenu?: () => void; // Add a ? to make it an optional prop
 };
 
 const Link = ({
-    page,
-    selectedPage,
-    setSelectedPage,
+  page,
+  selectedPage,
+  setSelectedPage,
+  isMobileMenuOpen,
+  closeMobileMenu,
 }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
 
-const lowerCasePage = page.toLocaleLowerCase().replace(/ /g, "") as SelectedPage;
+  const handleClick = () => {
+    if (isMobileMenuOpen && closeMobileMenu) {
+      // Close the mobile menu when a link is clicked
+      closeMobileMenu();
+    }
+    setSelectedPage(lowerCasePage);
+  };
 
   return (
     <AnchorLink
-        className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
-            transition duration-500 hover:text-primary-300
-            `}
-        href={`#${lowerCasePage}`}
-        onClick={() => setSelectedPage(lowerCasePage)}
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
+        transition duration-500 hover:text-primary-300
+        `}
+      href={`#${lowerCasePage}`}
+      onClick={handleClick}
     >
-        {page}
+      {page}
     </AnchorLink>
   );
 };
